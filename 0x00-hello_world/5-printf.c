86b853cf128a58260c626e8b21afef08fa161c67
ahmadabdul592 <ahmadabdul592@gmail.com>
2022-03-13 23:24:47 +0000

    C Programming Basics

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - prints the phrase with proper grammer.
- * but the outcome is a piece of art,
- * Return 0: if exited properly, non-zero otherwise
+ *main-Entry Point
+ *Return:Always 0 (Succes)
  */
 int main(void)
 {
